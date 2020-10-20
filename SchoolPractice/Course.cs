7d90448a85e8c7966b0744a7e7421b4ea76db84a
Michael Hacker <hack1973@swbell.net>
2020-10-20 16:21:42 -0500

    completed exercises

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Course
+    {
+        public string Topic { get; set; }
+        public Teacher Instructor { get; set; }
+        public List<Student> enrolledStudents { get; set; }
+
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,14 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student mike = new Student();
+            mike.Name = "Mike";
+            mike.NumberOfCredits = 1;
+            mike.Gpa = 4.0;
+            Console.WriteLine(mike.Name);
+            Console.WriteLine(mike.StudentId);
+            Console.WriteLine(mike.NumberOfCredits);
+            Console.WriteLine(mike.Gpa);
         }
     }
 }
